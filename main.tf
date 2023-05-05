4989955c5a9cf19d27e72aac0e26b279d4de3b84
Dias Saparov <dias@digger.dev>
2023-05-05 16:56:53 +0100

    bucket 3

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "null_resource" "test3" {
 resource "aws_s3_bucket" "test2" {
   bucket = "my-tf-test-bucket-2"
   tags = {
-    Name        = "My bucket 2"
+    Name        = "My bucket3"
     Environment = "Dev"
   }
 }
