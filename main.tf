c63825f36f552e0cc7cfacaf344373182f72be85
Dias Saparov <dias@digger.dev>
2023-05-05 16:02:26 +0100

    add bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 
 resource "null_resource" "test2" {
 }
+
+resource "aws_s3_bucket" "test2" {
+  bucket = "my-tf-test-bucket-2"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
