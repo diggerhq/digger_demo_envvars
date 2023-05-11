f1bd97f0f7903f6c0312233fab97aa5efe17aa79
Dias Saparov <dias@digger.dev>
2023-05-11 12:51:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "test3" {
 }
 
 resource "aws_s3_bucket" "test2" {
-  bucket = "delete-me-please-10-05-2023-09-49"
+  bucket = "delete-me-please-11-05-2023-12-51"
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
