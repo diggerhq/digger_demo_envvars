2db3cb24e8026f80b33560f290b2a86c1f4096f3
Dias Saparov <dias@digger.dev>
2023-07-05 10:39:36 +0100

    update to multi env

---
+++ dev/main.tf
@@ -0,0 +1,11 @@
+
+resource "null_resource" "test3" {
+}
+
+resource "aws_s3_bucket" "test2" {
+  bucket = "delete-me-please-dev-10-05-2023-09-49"
+  tags = {
+    Name        = "My bucket3"
+    Environment = "Dev"
+  }
+}
--- prod/main.tf
+++ prod/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "test3" {
 }
 
 resource "aws_s3_bucket" "test2" {
-  bucket = "delete-me-please-10-05-2023-09-49"
+  bucket = "delete-me-please-prod-10-05-2023-09-49"
   tags = {
     Name        = "My bucket3"
     Environment = "Dev"
